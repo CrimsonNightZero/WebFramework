6dad0ebfbb55698b0910f496635d1e949b05191c
Clark Chen <boo56mu@gmail.com>
2024-01-26 15:39:51 +0800

    feat: Throw customize exception with concrete exceptionHandler
    (cherry picked from commit 5177c8797122806d08cd451ac989780260b54be1)

--- HTTPProtocol/src/main/java/org/web/domain/ext/exceptions/NotExpectedExecutionHandler.java
+++ HTTPProtocol/src/main/java/org/web/domain/ext/exceptions/NotExpectedExecutionHandler.java
@@ -15,7 +15,7 @@ public class NotExpectedExecutionHandler extends ExceptionHandler {
     }
 
     @Override
-    protected HTTPResponse response(HTTPRequest httpRequest) {
+    protected HTTPResponse response(HTTPRequest httpRequest, Throwable throwable) {
         HTTPResponse httpResponse = new HTTPResponse(500);
         Map<String, String> headers = new HashMap<>();
         headers.put("content-type", "plain/text");
